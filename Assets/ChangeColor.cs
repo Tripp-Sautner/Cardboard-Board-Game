d03e0e4a49e3b582a7b67900b0ec8d17cc4dd31d
Tripp Sautner <52726452+Tripp-Sautner@users.noreply.github.com>
2019-12-20 22:33:30 -0500

    Full Game Upload
    First upload of the entire game

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        
+    }
+
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    public void Yellow()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
